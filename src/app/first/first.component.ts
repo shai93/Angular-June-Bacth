9d2263cc6c4905476ffd690af5d80dfcf2382cb6
shailesh <shailesh@Administrators-MacBook-Pro.local>
2021-07-20 20:54:22 +0530

    data binding

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -10,6 +10,29 @@ export class FirstComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  name = 'First Component';
+  name = 'Hello world';
   age: number = 12;
+  number: number = 0;
+  src: string =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Google_Images_2015_logo.svg/1200px-Google_Images_2015_logo.svg.png';
+
+  oneWayDataBinding: string = 'One Way Data binding !!!!!';
+
+  showMyName() {
+    this.name = 'Shailesh Salekar';
+  }
+
+  increment() {
+    this.number = this.number + 1;
+  }
+
+  decrement() {
+    if (this.number > 0) {
+      this.number = this.number - 1;
+    }
+  }
+
+  reset() {
+    this.number = 0;
+  }
 }
