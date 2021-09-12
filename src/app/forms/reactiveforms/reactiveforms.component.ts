0c78fd9f1d9f8d854221dcafec14ea8fbb33e5d6
shailesh <shailesh@Administrators-MacBook-Pro.local>
2021-09-12 20:12:03 +0530

    reactive form html

---
+++ src/app/forms/reactiveforms/reactiveforms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reactiveforms',
+  templateUrl: './reactiveforms.component.html',
+  styleUrls: ['./reactiveforms.component.scss']
+})
+export class ReactiveformsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
