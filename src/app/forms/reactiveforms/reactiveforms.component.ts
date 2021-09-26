273a6d32a7fb8c27806bbb3753451683e2b294a3
shailesh <shailesh@Administrators-MacBook-Pro.local>
2021-09-26 20:46:03 +0530

    Angular Reactive forms validation

--- src/app/forms/reactiveforms/reactiveforms.component.ts
+++ src/app/forms/reactiveforms/reactiveforms.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactiveforms',
   templateUrl: './reactiveforms.component.html',
-  styleUrls: ['./reactiveforms.component.scss']
+  styleUrls: ['./reactiveforms.component.scss'],
 })
 export class ReactiveformsComponent implements OnInit {
+  //dependency injection.
+  constructor(private fb: FormBuilder) {}
+  formSubmitted: boolean = false;
 
-  constructor() { }
+  UserRegistrationForm = this.fb.group({
+    userName: ['', Validators.required],
+    password: [],
+    confirmPassword: [],
+  });
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submitForm() {
+    this.formSubmitted = true;
   }
-
 }
+
+// service->class file
